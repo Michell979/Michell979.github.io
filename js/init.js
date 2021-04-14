90d7f700f45e6b17bb9f6446fa0a6080cd3032d5
Michell979 <michell5522698376@gmail.com>
2021-04-14 00:46:54 -0500

    quinto commit

--- js/init.js
+++ js/init.js
@@ -1,16 +1,7 @@
+
 // @ts-ignore
 firebase.initializeApp({
-  <!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/8.4.1/firebase-analytics.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+    var firebaseConfig = {
     apiKey: "AIzaSyAailp7Qof9xSk-6HA8TobxraAT1FaHJbk",
     authDomain: "michellpwa.firebaseapp.com",
     projectId: "michellpwa",
@@ -18,11 +9,4 @@ firebase.initializeApp({
     messagingSenderId: "1010456134537",
     appId: "1:1010456134537:web:0dc07d58bcd1489c691758",
     measurementId: "G-2FH05VKGF8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
 });
-
-
